24188797902c6708b40d56d838650459de4c7766
Naski <github@naski.org>
2023-08-06 17:17:12 +0200

    deployment fix

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,6 +1,6 @@
 export default function Rating(props: { rating: number }) {
   return (
-    <div className="flex px-1 py-2 rounded-full bg-background select-none">
+    <div className="flex p-2 rounded-xl bg-background select-none">
       {props.rating ? (
         <p className={`text-xl text-light font-bold`}>{props.rating}</p>
       ) : (
